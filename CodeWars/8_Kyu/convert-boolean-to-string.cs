c29fab8de5d8fdf3056d204118bb8d48b6458fef
mazeTemporal <farrelbd@gmail.com>
2018-10-26 09:26:28 -0400

    completed Code Wars 8 Kyu: convert boolean to string

---
+++ CodeWars/8_Kyu/convert-boolean-to-string.cs
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/convert-boolean-values-to-strings-yes-or-no/train/csharp
+
+/*
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+
+using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static string boolToWord(bool word)
+  {
+    return(word ? "Yes" : "No");
+  }
+}
+
