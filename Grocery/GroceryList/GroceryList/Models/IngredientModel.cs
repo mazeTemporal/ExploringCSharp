9006159e4d503100c0064fc3dffe8da03df41b4d
mazeTemporal <mazeTemporal@users.noreply.github.com>
2019-04-10 15:37:20 -0400

    Grocery List: created basic view Models

---
+++ Grocery/GroceryList/GroceryList/Models/IngredientModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GroceryList.Models
+{
+  public class IngredientModel
+  {
+    public enum UnitType
+    {
+      Gram,
+      Millileter,
+      Liter,
+      Ounce,
+      Pound,
+      Fluid_Ounce,
+      Teaspoon,
+      Tablespoon,
+      Cup,
+      Pint,
+      Quart,
+      Gallon
+    }
+
+    [Required]
+    public string Ingredient { get; set; }
+
+    [Required]
+    public double Amount { get; set; }
+
+    [Required]
+    public UnitType Unit { get; set; }
+
+    [Required]
+    public string Category { get; set; }
+  }
+}
---
+++ Grocery/GroceryList/GroceryList/Models/RecipeModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace GroceryList.Models
+{
+  public class RecipeModel
+  {
+    [Required]
+    public string Recipe { get; set; }
+
+    public List<IngredientModel> Ingredients;
+
+    public string PDF { get; set; }
+  }
+}
