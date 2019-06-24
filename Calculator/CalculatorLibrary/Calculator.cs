58b47e80e92ad1915ab9c21dc25da057dd9d3a7c
mazeTemporal <mazeTemporal@users.noreply.github.com>
2019-06-24 17:51:28 -0400

    Calculator: library - created stubs for calculator functions

--- Calculator/CalculatorLibrary/Calculator.cs
+++ Calculator/CalculatorLibrary/Calculator.cs
@@ -6,5 +6,112 @@ namespace CalculatorLibrary
 {
     public class Calculator
     {
+        public enum Operation
+        {
+            None,
+            Add,
+            Subtract,
+            Multiply,
+            Divide
+        };
+        public double CurrentValue { get; set; } = 0;
+        public double PreviousValue { get; set; } = 0;
+        public double MemoryValue { get; set; } = 0;
+        public Operation CurrentOperation { get; set; } = Operation.None;
+
+        public void AppendDigit(int digit)
+        {
+            //!!! stub
+        }
+
+        public void AppendDecimal()
+        {
+            //!!! stub
+        }
+
+        public void RemoveLastDigit()
+        {
+            //!!! stub
+        }
+
+        public void Add()
+        {
+            //!!! stub
+        }
+
+        public void Subtract()
+        {
+            //!!! stub
+        }
+
+        public void Multiply()
+        {
+            //!!! stub
+        }
+
+        public void Divide()
+        {
+            //!!! stub
+        }
+
+        public void MultiplicitiveInverse()
+        {
+            //!!! stub
+        }
+
+        public void AdditiveInverse()
+        {
+            //!!! stub
+        }
+
+        public void Percent()
+        {
+            //!!! stub
+        }
+
+        public void Calculate()
+        {
+            //!!! stub
+        }
+
+        public void SquareRoot()
+        {
+            //!!! stub
+        }
+
+        public void ClearEntry()
+        {
+            //!!! stub
+        }
+
+        public void Clear()
+        {
+            //!!! stub
+        }
+
+        public void MemorySave()
+        {
+            //!!! stub
+        }
+
+        public void MemoryClear()
+        {
+            //!!! stub
+        }
+
+        public void MemoryReturn()
+        {
+            //!!! stub
+        }
+
+        public void MemoryAdd()
+        {
+            //!!! stub
+        }
+
+        public void MemorySubtract()
+        {
+            //!!! stub
+        }
     }
 }
